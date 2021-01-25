e66b59a8bdc6e9e069b8535700d29bcf7c183532
lmikoto <lmikoto@outlook.com>
2021-01-25 22:38:55 +0800

    type

--- src/main/java/io/github/lmikoto/CopyInvokeDubbo.java
+++ src/main/java/io/github/lmikoto/CopyInvokeDubbo.java
@@ -10,6 +10,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiType;
+import com.intellij.psi.impl.source.PsiClassReferenceType;
 import com.intellij.psi.impl.source.PsiMethodImpl;
 import com.intellij.psi.impl.source.PsiParameterImpl;
 import org.jetbrains.annotations.NotNull;
@@ -18,10 +19,8 @@ public class CopyInvokeDubbo extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-
         Editor editor = e.getDataContext().getData(CommonDataKeys.EDITOR);
         PsiFile psiFile = e.getDataContext().getData(CommonDataKeys.PSI_FILE);
-        Project project = editor.getProject();
         PsiElement referenceAt = psiFile.findElementAt(editor.getCaretModel().getOffset());
         PsiElement parent = referenceAt.getParent();
         if(parent instanceof  PsiMethodImpl){
@@ -30,8 +29,8 @@ public class CopyInvokeDubbo extends AnAction {
             for(JvmParameter p : parameters){
                 PsiParameterImpl pImpl = (PsiParameterImpl)p;
                 String name = pImpl.getName();
-                PsiType type = pImpl.getType();
-                PsiElement identifyingElement = pImpl.getIdentifyingElement();
+                PsiClassReferenceType type = (PsiClassReferenceType)pImpl.getType();
+                String typeClassName = type.getClassName();
                 System.out.println(pImpl);
             }
             System.out.println(method);
